260b73e508e09890efa1b8bb5f10c477cd0cb269
Gustavo Luque <gus_luqe@live.com.ar>
2023-12-27 00:30:01 -0300

    modifique fondo y boton

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,1 @@
-const colores = ['bluesky', 'green', 'yellowgreen','darkblue','purple', 'orange', 'brown', 'black']
 
-
-const btn = document.getElementById('btn')
-
-btn.onclick = function () {
-    document.body.style.backgroundColor = colores[Math.floor(Math.random() * colores.length)]
-}
